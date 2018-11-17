bed7c882936915ad37382513e94c8814bacb86f9
Hutaillefu <hugo.taillefumier@yahoo.fr>
2018-11-17 12:19:03 +0100

    move parser func

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   parser.c                                         .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: htaillef <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/18 19:23:40 by htaillef     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/18 19:23:42 by htaillef    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
 
 /*
   ** Read one line as int and set the number of ants
@@ -19,6 +31,34 @@ int	parse_num_ants(t_world *world)
 	free(line);
 	line = NULL;
 	return (1);
+}
+
+/*
+  ** Parse the string data {name, x, y} into room object.
+  ** x & y must be int.
+*/
+
+t_room	*parse_room(const char *line)
+{
+	char	**values;
+	t_room	*room;
+
+	values = NULL;
+	room = NULL;
+	if (!(values = ft_strsplit(line, ' ')))
+		return (NULL);       
+	if (ft_tablen(values) != 3 || !ft_strisdigit(values[1]) || !ft_strisdigit(values[2]))
+	{
+		ft_free_tab(values);
+		return (NULL);
+	}
+	if (!(room = create_room(values[0], ft_atoi(values[1]), ft_atoi(values[2]))))
+	{
+		ft_free_tab(values);
+		return (NULL);
+	}
+	ft_free_tab(values);
+	return (room);
 }
 
 /*
@@ -62,3 +102,36 @@ int		parse_active_commentary(t_world *world, const char *pre_line)
 	line = NULL;
 	return (1);
 }
+
+int	parse_link(const char *line, t_world *world)
+{
+	char 	**values;
+	int	start_index;
+	int	end_index;
+
+	if (!line || !world)
+		return (0);
+	if (!(world->links) && !init_links(world))
+		return (0);
+	if (!(values = ft_strsplit(line, '-')))
+		return (0);
+	if (ft_tablen(values) != 2)
+	{
+		ft_free_tab(values);
+	       	return (0);
+	}
+	start_index = get_room_index(world, values[0]);
+	end_index = get_room_index(world, values[1]);
+	
+	if (start_index < 0 || end_index < 0 || 
+		!is_room_name_exist(world, values[0]) ||
+		!is_room_name_exist(world, values[1]) ||
+		!add_link(world, start_index, end_index))
+	{
+		ft_free_tab(values);
+		return (0);
+	}
+	ft_free_tab(values);
+	return (1);
+}
+
