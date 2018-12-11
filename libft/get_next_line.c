73da8d4985bffa084ef7823ca9a057215d141e14
Hugo Taillefumier <htaillef@z4r8p6.le-101.fr>
2018-12-11 19:00:08 +0100

    no leaks

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -13,112 +13,57 @@
 
 #include "libft.h"
 
-int			index_of(const char *s, int c)
+char		*ft_readline(const int fd, char *buff, int *ret)
 {
-	int i;
+	char	tmp[BUFF_SIZE + 1];
+	char	*tmp2;
 
-	i = 0;
-	while (s[i])
+	if ((*ret = read(fd, tmp, BUFF_SIZE)) < 0)
+		return (0);
+	tmp[*ret] = 0;
+	tmp2 = buff;
+	if (!(buff = ft_strjoin(buff, tmp)))
 	{
-		if (s[i] == c)
-			return (i);
-		i++;
+		free(buff);
+		return (0);
 	}
-	return (-1);
+	free(tmp2);
+	return (buff);
 }
 
-char		*process(char **stock)
+int			ft_cpy_end(char **line, char **buff)
 {
-	int		pos;
-	char	*res;
-	char	*stock_ret;
-
-	if ((pos = index_of(*stock, '\n')) >= 0)
-	{
-		stock_ret = *stock;
-		if (!(res = ft_strsub(*stock, 0, pos)) ||
-				!(*stock = ft_strsub(*stock, pos + 1,
-						ft_strlen(*stock) - pos + 1)))
-			return (NULL);
-		free(stock_ret);
-		stock_ret = NULL;
-		return (res);
-	}
-	return (NULL);
-}
-
-int			read_line(int fd, char **stock, char **line, char **res)
-{
-	char			buffer[BUFF_SIZE + 1];
-	int				ret;
-	char			*stock_ret;
-
-	while ((ret = read(fd, buffer, BUFF_SIZE)))
-	{
-		buffer[ret] = '\0';
-		stock_ret = *stock;
-		if (ret == -1 || !(*stock = ft_strjoin(*stock, buffer)))
-			return (-1);
-		ft_strdel(&stock_ret);
-		if ((*res = process(stock)))
-		{
-			*line = *res;
-			return (1);
-		}
-	}
-	if (!(*res) && ft_strlen(*stock) > 0)
-	{
-		if (!(*line = ft_strdup(*stock)))
-			return (-1);
-		ft_strdel(stock);
-		return (1);
-	}
-	return (0);
-}
-
-t_list		*get_lst(t_list **lst, size_t fd)
-{
-	t_list		*iterator;
-	t_list		*new;
-
-	iterator = *lst;
-	while (iterator)
-	{
-		if (iterator->content_size == fd)
-		{
-			return (iterator);
-		}
-		iterator = iterator->next;
-	}
-	new = ft_lstnew(NULL, 0);
-	new->content_size = fd;
-	ft_lstadd(lst, new);
-	return (*lst);
+	if (!(*line = ft_strdup(*buff)))
+		return (-1);
+	ft_bzero(*buff, ft_strlen(*buff));
+	return (1);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	static t_list	*list = NULL;
-	char			*res;
-	t_list			*current;
+	static char		*buff = NULL;
+	int				ret;
+	char			*str;
 
-	res = NULL;
-	current = NULL;
-	if (fd < 0 || !(line))
+	ret = 1;
+	if (!line || fd < 0 || BUFF_SIZE < 1 || (!buff && !(buff = ft_memalloc(1))))
 		return (-1);
-	current = get_lst(&list, (size_t)fd);
-	if (!current)
-		return (-1);
-	if ((char *)current->content)
+	while (ret > 0)
 	{
-		if ((res = process((char **)&current->content)))
+		if ((str = ft_strchr(buff, '\n')))
 		{
-			*line = res;
+			*str = 0;
+			if (!(*line = ft_strdup(buff)))
+				return (-1);
+			ft_strcpy(buff, str + 1);
 			return (1);
 		}
-	}
-	if (!(char *)current->content)
-		if (!(char *)(current->content = ft_strdup("")))
+		if (!(buff = ft_readline(fd, buff, &ret)))
 			return (-1);
-	return (read_line(fd, (char **)&current->content, line, &res));
+	}
+	free(str);
+	if (!ret && ft_strlen(buff))
+		ret = ft_cpy_end(line, &buff);
+	(!(*buff)) ? ft_memdel((void *)&buff) : 0;
+	return (ret);
 }
